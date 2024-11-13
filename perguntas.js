4192ab10651d94e2e44c0cbedaf2d755e90dd6c7
dudabroseira <00001095544378sp@al.educacao.sp.gov.br>
2024-11-13 08:10:20 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Geral',
+    'Qual foi a primeira rede social a ganhar popularidade no mundo?',
+    'MySpace.'
+)
+
+criaCartao(
+    'Geral',
+    'Qual bebida é feita a partir de uvas fermentadas?',
+    'Vinho.'
+)
+
+criaCartao(
+    'Geral',
+    'Em qual país se originou o carnaval moderno, famoso por desfiles e fantasias?',
+    'Brasil.'
+)
+
+criaCartao(
+    'Geral',
+    'Quem pintou a obra "Mona Lisa"?',
+    'Leonardo da Vinci.'
+)
